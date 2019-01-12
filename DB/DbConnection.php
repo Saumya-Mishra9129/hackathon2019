bdb1a0b8460065f78cfb5f4069f9509a3973079c
rakshak1010 <rakshak.malhotra48@gmail.com>
2019-01-12 17:20:28 +0530

    connection updated

--- DB/DbConnection.php
+++ DB/DbConnection.php
@@ -1,16 +1,16 @@
 <?php
-$user = "root";
-$password = "rootuser";
+$user = "hackathon";
+$password = "12345";
 $host = "localhost";
 
 //connection to the database
 //$dbhandle = mysql_connect($hostname, $username, $password)
-$connection = mysqli_connect('localhost', 'root', 'rootuser','BloodBank');
+$connection = mysqli_connect('localhost', 'hackathon', '12345','HackFest');
  //or die(mysql_error());
 
 //select a database to work 
-$db = "BloodBank";
-$selected = mysqli_select_db($connection,"BloodBank") 
+$db = "HackFest";
+$selected = mysqli_select_db($connection,"HackFest") 
   or die(mysqli_error($selected));
 ?>
 
